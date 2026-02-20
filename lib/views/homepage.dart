ea4a447adab463a8e6fd262f46485b239912dfd8
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-21 05:15:46 +0700

    feat: ubah gradasi warna latar belakang

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -31,62 +31,83 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Consumer<CounterModel>(
       builder: (context, value, child) {
-        return Scaffold(
-          appBar: AppBar(
-            // TRY THIS: Try changing the color here to a specific color (to
-            // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-            // change color while the other colors stay the same.
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: Text(widget.title),
+        return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: AlignmentGeometry.bottomRight,
+              colors: [Color(0xFF1A3263), Color(0xFF547792)],
+            ),
           ),
-          body: Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            child: Column(
-              // Column is also a layout widget. It takes a list of children and
-              // arranges them vertically. By default, it sizes itself to fit its
-              // children horizontally, and tries to be as tall as its parent.
-              //
-              // Column has various properties to control how it sizes itself and
-              // how it positions its children. Here we use mainAxisAlignment to
-              // center the children vertically; the main axis here is the vertical
-              // axis because Columns are vertical (the cross axis would be
-              // horizontal).
-              //
-              // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-              // action in the IDE, or press "p" in the console), to see the
-              // wireframe for each widget.
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text('You have pushed the button this many times:'),
-                Text(
-                  value.angka.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+
+            appBar: AppBar(
+              // TRY THIS: Try changing the color here to a specific color (to
+              // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+              // change color while the other colors stay the same.
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              // Here we take the value from the MyHomePage object that was created by
+              // the App.build method, and use it to set our appbar title.
+              title: Text(
+                widget.title,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            body: Center(
+              // Center is a layout widget. It takes a single child and positions it
+              // in the middle of the parent.
+              child: Column(
+                // Column is also a layout widget. It takes a list of children and
+                // arranges them vertically. By default, it sizes itself to fit its
+                // children horizontally, and tries to be as tall as its parent.
+                //
+                // Column has various properties to control how it sizes itself and
+                // how it positions its children. Here we use mainAxisAlignment to
+                // center the children vertically; the main axis here is the vertical
+                // axis because Columns are vertical (the cross axis would be
+                // horizontal).
+                //
+                // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+                // action in the IDE, or press "p" in the console), to see the
+                // wireframe for each widget.
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                  Text(
+                    value.angka.toString(),
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            floatingActionButton: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    value.increment();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(width: 10),
+                FloatingActionButton(
+                  onPressed: () {
+                    value.decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
                 ),
               ],
             ),
-          ),
-          floatingActionButton: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  value.increment();
-                },
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ),
-              const SizedBox(width: 10),
-              FloatingActionButton(
-                onPressed: () {
-                  value.decrement();
-                },
-                tooltip: 'Decrement',
-                child: const Icon(Icons.remove),
-              ),
-            ],
           ),
         );
       }, // This trailing comma makes auto-formatting nicer for build methods.
