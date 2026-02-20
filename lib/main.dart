0b8fd9ac50ffe129f834728a3a3bd910646855d0
Satriahrl_ <satriaherlambang22@gmail.com>
2026-02-21 03:02:35 +0700

    register providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:stateman/homepage.dart';
+import 'package:provider/provider.dart';
+import 'counter_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
